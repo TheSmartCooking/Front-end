43dc733b52514791ea8a8a8574936312e392b3f4
Vianpyro <vianney@veremme.org>
2024-11-29 14:44:27 -0500

    Refactor modal component: rename close button ID, enhance modal functionality, and add event listeners for opening and closing

---
+++ javascript/modal.js
@@ -0,0 +1,33 @@
+const modalContainer = document.getElementById('modal-container');
+const modalBox = document.getElementById('modal-box');
+const closeButton = document.getElementById('modal-close');
+
+
+closeButton.addEventListener('click', () => {
+    closeModal();
+});
+
+window.addEventListener('click', function (e) {
+    if (!modalBox.contains(e.target) && modalContainer.contains(e.target)) {
+        closeModal();
+    }
+});
+
+function closeModal() {
+    enableScroll();
+    modalContainer.style.display = 'none';
+    resetModal();
+}
+
+function disableScroll() {
+    document.body.style.overflow = 'hidden';
+}
+
+function enableScroll() {
+    document.body.style.overflow = 'auto';
+}
+
+function openModal() {
+    disableScroll();
+    modalContainer.style.display = 'grid';
+}
