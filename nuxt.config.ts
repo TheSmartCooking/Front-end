bf99c170cd16f9e4f5fb8daa1c125d5de1986720
Vianpyro <vianney@veremme.org>
2025-01-06 08:39:35 -0500

    Disable SSR

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
+    css: [
+        '@/assets/styles/variables.css'
+    ],
     devtools: { enabled: false },
     runtimeConfig: {
         public: { appTitle: 'Smart Cooking &copy;' }
     },
-    css: [
-        '@/assets/styles/variables.css'
-    ]
+    ssr: false
 })
