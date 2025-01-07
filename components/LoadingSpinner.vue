bbc4f98a48e216159d29b97be3c3840479b97d0a
Vianpyro <vianney@veremme.org>
2025-01-06 21:20:36 -0500

    Refactor LoadingSpinner styles for improved readability and consistency

--- components/LoadingSpinner.vue
+++ components/LoadingSpinner.vue
@@ -6,31 +6,26 @@
 
 <style scoped>
 aside {
-    position: fixed;
-    inset: 0;
-    display: flex;
-    justify-content: center;
     align-items: center;
     background-color: rgba(0 0 0 / 50%);
+    display: flex;
+    justify-content: center;
+    inset: 0;
+    position: fixed;
     z-index: 1000;
 }
 
 span {
-    width: 7rem;
-    height: 7rem;
-    border: 1rem solid #888;
-    border-top: 1rem solid var(--polaroid-background-color);
-    border-radius: 50%;
     animation: spin 1s linear infinite;
+    border: 1rem solid light-dark(var(--color-primary), var(--color-quinary));
+    border-radius: 50%;
+    border-top: 1rem solid transparent;
+    height: 7rem;
+    width: 7rem;
 }
 
 @keyframes spin {
-    0% {
-        transform: rotate(0deg);
-    }
-
-    100% {
-        transform: rotate(360deg);
-    }
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
 }
 </style>
