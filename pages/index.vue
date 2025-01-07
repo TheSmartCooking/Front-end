4f26b469375f2d07f44264442096c3680d9c1f6f
Vianpyro <vianney@veremme.org>
2025-01-06 19:28:55 -0500

    Refactor index.vue template structure for improved readability and layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,13 @@
 <template>
-    <main>
-        <p v-if="error">Unable to load recipes, please try again later or contact support</p>
-        <p v-else-if="!recipes">Loading recipes...</p>
-        <p v-else-if="!recipes.data">No recipes available</p>
-        <section v-else id="recipes">
-            <RecipeCard
-                v-for="recipe in recipes.data"
-                :key="recipe.recipe_id"
-                :recipe="recipe"
-            />
-        </section>
+    <p v-if="error">Unable to load recipes, please try again later or contact support</p>
+    <p v-else-if="!recipes">Loading recipes...</p>
+    <p v-else-if="!recipes.data">No recipes available</p>
+    <main v-else>
+        <RecipeCard
+            v-for="recipe in recipes.data"
+            :key="recipe.recipe_id"
+            :recipe="recipe"
+        />
     </main>
 </template>
 
@@ -27,18 +25,15 @@ const { data: recipes, _, error } = useFetch(`${config.public.apiBaseUrl}/recipe
 
 <style scoped>
 main {
-    overflow: hidden auto;
-    scrollbar-color: var(--color-quinary) transparent;
-    scrollbar-width: thin;
-}
-
-#recipes {
     display: flex;
     flex-wrap: wrap;
     gap: var(--spacing-medium);
     justify-content: center;
     justify-self: center;
-    width: 100%;
+    overflow: hidden auto;
+    scrollbar-color: var(--color-quinary) transparent;
+    scrollbar-width: thin;
     text-wrap: nowrap;
+    width: 100%;
 }
 </style>
