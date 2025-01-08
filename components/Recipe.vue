e0a2028eb7c1c1f42441a6ff677dd3433a76a80c
Vianpyro <vianney@veremme.org>
2025-01-08 12:07:21 -0500

    Refactor recipe details display into a separate Recipe component

---
+++ components/Recipe.vue
@@ -0,0 +1,23 @@
+<template>
+    <p v-if="recipe.title"><strong>Title:</strong> {{ recipe.title }}</p>
+    <p v-if="recipe.author_name"><strong>Author:</strong> {{ recipe.author_name }}</p>
+    <p v-if="recipe.cook_time"><strong>Cook Time:</strong> {{ recipe.cook_time }} minutes</p>
+    <p v-if="recipe.details"><strong>Details:</strong> {{ recipe.details }}</p>
+    <p v-if="recipe.difficulty_level"><strong>Difficulty Level:</strong> {{ recipe.difficulty_level }}</p>
+    <p v-if="recipe.number_of_reviews"><strong>Number of Reviews:</strong> {{ recipe.number_of_reviews }}</p>
+    <p v-if="recipe.nutritional_information"><strong>Nutritional Information:</strong> {{ recipe.nutritional_information }}</p>
+    <p v-if="recipe.picture_id"><strong>Picture ID:</strong> {{ recipe.picture_id }}</p>
+    <p v-if="recipe.preparation"><strong>Preparation:</strong> {{ recipe.preparation }}</p>
+    <p v-if="recipe.recipe_status"><strong>Status:</strong> {{ recipe.recipe_status }}</p>
+    <p v-if="recipe.video_url"><strong>Video URL:</strong> <a :href="recipe.video_url" target="_blank">{{ recipe.video_url }}</a></p>
+</template>
+
+<script setup>
+// Props
+const { recipe } = defineProps({
+    recipe: {
+        type: Object,
+        required: true,
+    },
+});
+</script>
--- pages/recipe/[id].vue
+++ pages/recipe/[id].vue
@@ -1,17 +1,13 @@
 <template>
-    <main>
-        <p v-if="recipe.title"><strong>Title:</strong> {{ recipe.title }}</p>
-        <p v-if="recipe.author_name"><strong>Author:</strong> {{ recipe.author_name }}</p>
-        <p v-if="recipe.cook_time"><strong>Cook Time:</strong> {{ recipe.cook_time }} minutes</p>
-        <p v-if="recipe.details"><strong>Details:</strong> {{ recipe.details }}</p>
-        <p v-if="recipe.difficulty_level"><strong>Difficulty Level:</strong> {{ recipe.difficulty_level }}</p>
-        <p v-if="recipe.number_of_reviews"><strong>Number of Reviews:</strong> {{ recipe.number_of_reviews }}</p>
-        <p v-if="recipe.nutritional_information"><strong>Nutritional Information:</strong> {{ recipe.nutritional_information }}</p>
-        <p v-if="recipe.picture_id"><strong>Picture ID:</strong> {{ recipe.picture_id }}</p>
-        <p v-if="recipe.preparation"><strong>Preparation:</strong> {{ recipe.preparation }}</p>
-        <p v-if="recipe.recipe_status"><strong>Status:</strong> {{ recipe.recipe_status }}</p>
-        <p v-if="recipe.video_url"><strong>Video URL:</strong> <a :href="recipe.video_url" target="_blank">{{ recipe.video_url }}</a></p>
-    </main>
+    <div id="recipe-details">
+        <Recipe
+            :key="recipe.recipe_id"
+            :recipe="recipe"
+        />
+    </div>
+    <div id="similar-recipes">
+        <!-- TODO -->
+    </div>
 </template>
 
 <script setup>
