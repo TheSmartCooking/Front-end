f621c23de35eb2b6c196e756a2d92c3e9589fe05
Vianpyro <vianney@veremme.org>
2025-01-31 11:19:34 -0500

    Remove console log and simulated network delay from fetchJSON function

--- utils/fetch.js
+++ utils/fetch.js
@@ -1,11 +1,7 @@
 import { showSpinner, hideSpinner } from "./spinner";
 
 export async function fetchJSON(url) {
-    console.log("fetching JSON data from", url);
     showSpinner();
-
-    // Simulate a network delay
-    await new Promise((resolve) => setTimeout(resolve, 1500));
 
     try {
         let response = await fetch(url);
