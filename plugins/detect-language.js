81b2db7a7482538df5edf70749205dd08670ff7e
Vianpyro <vianney@veremme.org>
2025-01-31 11:18:21 -0500

    Fix cookie maxAge calculation for language detection plugin

--- plugins/detect-language.js
+++ plugins/detect-language.js
@@ -7,7 +7,7 @@ export default defineNuxtPlugin({
         // Check if the cookie already exists
         const languageCookie = useCookie("language_code", {
             sameSite: 'strict',
-            maxAge: 60 * 60 * 24 * 30 * 100 // 100 years
+            maxAge: 60 * 60 * 24 * 365 * 100 // 100 years
         });
 
         if (!languageCookie.value) {
