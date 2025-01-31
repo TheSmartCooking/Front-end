491a2cd48274eea3a3c9a4c66bbe0346720031f8
Vianpyro <vianney@veremme.org>
2025-01-31 11:10:16 -0500

    Add language detection plugin and enhance fetch logging

--- utils/fetch.js
+++ utils/fetch.js
@@ -1,7 +1,12 @@
 import { showSpinner, hideSpinner } from "./spinner";
 
 export async function fetchJSON(url) {
+    console.log("fetching JSON data from", url);
     showSpinner();
+
+    // Simulate a network delay
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+
     try {
         let response = await fetch(url);
         let data = await response.json();
