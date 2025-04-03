25c37d1927a8c3b9a41b1b9d8713b425a36a9c13
Vianpyro <vianney@veremme.org>
2025-04-03 21:10:22 +0000

    Refactor login and registration links for improved readability and structure

--- pages/login.vue
+++ pages/login.vue
@@ -2,17 +2,17 @@
     <div id="authentication">
         <section id="login">
             <Login />
-            <a id="go-to-login" @click="goToRegister"
-                >Don't have an account?
-                <span class="link-color">Register</span></a
-            >
+            <a id="go-to-login" @click="goToRegister">
+                Don't have an account?
+                <span class="link-color">Register</span>
+            </a>
         </section>
         <section id="register" class="hidden">
             <Register :goToLogin="goToLogin" />
-            <a id="go-to-register" @click="goToLogin"
-                >Already have an account?
-                <span class="link-color">Login</span></a
-            >
+            <a id="go-to-register" @click="goToLogin">
+                Already have an account?
+                <span class="link-color">Login</span>
+            </a>
         </section>
     </div>
 </template>
@@ -50,8 +50,6 @@ section {
 #authentication {
     display: flex;
     align-items: center;
-    justify-content: center;
-    height: 100%;
 }
 
 :deep(button[type='submit']) {
